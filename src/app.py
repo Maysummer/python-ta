2cf29a3812bd60f6ed44e4f7d8f8fb7065fa6d99
Maysummer <42492293+Maysummer@users.noreply.github.com>
2021-01-31 23:35:39 +0100

    Add files via upload
    app commit

--- src/app.py
+++ src/app.py
@@ -1,9 +1,14 @@
 import sys
 import os
+import string
 
 
 def palindrome(s):
     # your code goes here
+    possible = set(string.ascii_lowercase)
+    s = s.lower()
+    s = ''.join([char for char in s if char in possible])
+    return s == s[::-1]
 
 def solution(s):
     return palindrome(s)
